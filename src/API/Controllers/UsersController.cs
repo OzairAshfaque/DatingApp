83224c54f864edd32f16f84a52a2da25ae8462f5
Ozair <ozair.ashfaque@gmail.com>
2021-03-10 12:45:03 +1100

    Async Task Added in ActionResult of GetUser

--- src/API/Controllers/UsersController.cs
+++ src/API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace src.API.Controllers
@@ -19,20 +21,19 @@ namespace src.API.Controllers
             Configuration = config;
         }
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-         return  _context.Users.ToList();
+         return  await _context.Users.ToListAsync();
      
         
 
     }
-    // [HttpGet("{id}")]
-    // public ActionResult<AppUser> GetUser(int id)
-    // {
-    //     return _context.Users.Find(id);
-         
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
 
-    // }
+    }
         
     }
 }
